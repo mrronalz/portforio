7ceb5a085d1b98e62ea6d39e2c34631e85807e74
mrronalz <mrronalz@gmail.com>
2016-12-16 12:23:35 +0700

    Add Port

---
+++ js/function.js
@@ -0,0 +1,41 @@
+
+var workBoxWidthRe = $('.work .workbox').width();
+
+$('.work .workbox , #traineetracker').css('height', workBoxWidthRe);
+
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1) {
+        $(".header").addClass("scroll");
+    } else {
+        $(".header").removeClass("scroll");
+    }
+});
+
+$(window).resize(function(event) {
+  var workBoxWidthRe = $('.work .workbox').width();
+
+  $('.work .workbox , #traineetracker').css('height', workBoxWidthRe);
+  /*if( $(window).width() < 999 ) {
+    $('.work .workbox').css('width', ($(window).width()/2)-20);
+  }*/
+});
+
+$('#worklink').click(function() {
+    $('html, body').animate({
+        scrollTop: $('#work').offset().top
+    }, 500);
+});
+
+$('#aboutlink').click(function() {
+    $('html, body').animate({
+        scrollTop: $('#about').offset().top
+    }, 500);
+});
+
+$('.burgermenu').click (function(){
+  $(this).toggleClass('open');
+  $('.header').toggleClass('open');
+});
